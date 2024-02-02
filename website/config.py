c7ee7d66ee7a53d48af97b7264a1682d9fa2b0f2
Ján Pullmann <john.pullmann@gmail.com>
2024-02-02 14:59:53 +0100

    Update settings and credentials obtaining in config.py

--- website/config.py
+++ website/config.py
@@ -1,13 +1,14 @@
 import os
+from website import settings
 
 class Config:
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = settings.SQLALCHEMY_DATABASE_URI
+
+    SECRET_KEY = settings.SECRET_KEY
     
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_SERVER = settings.MAIL_SERVER
+    MAIL_PORT = settings.MAIL_PORT
+    MAIL_USE_TLS = settings.MAIL_USE_TLS
+    MAIL_USERNAME = settings.MAIL_USERNAME
+    MAIL_PASSWORD = settings.MAIL_PASSWORD
